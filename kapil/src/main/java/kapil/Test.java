2857af491b0495f956d235253916f56bbbcc52e9
krana10 <100963817+krana10@users.noreply.github.com>
2022-04-08 08:17:12 +0530

    Adding comment

--- kapil/src/main/java/kapil/Test.java
+++ kapil/src/main/java/kapil/Test.java
@@ -10,15 +10,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test {
 
 	public static void main(String[] args) {
+
+		// Launch the Browser
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+
+		// Window maximize
 		driver.manage().window().maximize();
+
+		// Open URL
 		driver.get("https://www.moneycorp.com/en-gb/");
+
+		// Change Language and region
 		WebElement languageList = driver.findElement(By.id("language-dropdown-flag"));
 		languageList.click();
-
 		driver.findElement(By.xpath("//*[@id=\"nav-languages-overlay\"]/div/ul/li[9]/a")).click();
 
+		// Validate Foreign exchange solutions
 		WebElement foreignExchangeSolutions = driver
 				.findElement(By.xpath("/html/body/section/div[4]/div[3]/div/div/div[1]/div/a"));
 		JavascriptExecutor jse = (JavascriptExecutor) driver;
@@ -30,6 +38,7 @@ public class Test {
 
 		driver.findElement(By.xpath("/html/body/section/div[1]/header/div[2]/div/div[3]/button[1]")).click();
 
+		// International payments searching
 		WebElement navSearch = driver.findElement(By.id("nav_search"));
 		navSearch.sendKeys("international payments");
 		navSearch.sendKeys(Keys.ENTER);
